87f46efb6bee3a3818e1dd06c1245f154c3cea34
rence <99105241+rencejke@users.noreply.github.com>
2023-12-10 14:56:40 +0800

    Delete Source-Code directory

--- Source-Code/Source Code/QuizDataScriptable.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "QuestionData", menuName = "QuestionData")]
-public class QuizDataScriptable : ScriptableObject
-{
-    public List <Question> questions;
-}
